ba623133d64c73c8a2d33171f259360071e8d8e9
Sara <s.buhktari@tudelft.nl>
2026-05-26 12:05:53 +0200

    added negative integers and test for daily_max

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,15 +29,32 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
-from inflammation.models import daily_mean, daily_max
+from inflammation.models import daily_mean, daily_max, daily_min
 
 def test_daily_max_integers():
-    """Test that the max function works for an array of positive integers.
+    """Test that the max function works for an array of positive and negative integers.
     """
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
+    # # test_input = np.array([[1, 2, -9], [-3, 4, -2], [-1, 5, -6]])
+    # # test_result = np.array([1, 5, -2])  
+    # #  
+    # # test_input = np.array([[1, 2, -8],
+    # #                        [3, 4, -1],
+    # #                        [5, 6, -2]])
     
-    test_result = np.array([5, 6])
+    # # test_result = np.array([5, 6, -3])
+
+    # test_input = np.array([[1, 2, -9], [-3, 4, -2], [-1, 5, -6]])
+    # test_result = np.array([5, 6, -3], [-3, 4, -2], [-1, 5, -6]])              # update test_output accordingly
+
+    # npt.assert_array_equal(daily_max(test_input), test_result)
+
+    test_input = np.array([
+        [1, 2, -9],
+        [-3, 4, -2],
+        [-1, 5, -6]
+    ])
+
+    test_result = np.array([1, 5, -2])
+
     npt.assert_array_equal(daily_max(test_input), test_result)
+
